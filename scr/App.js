7d6e7bfe7f86b390101abe68ef28326c87fab992
totquf <dalda117@naver.com>
2023-02-20 23:12:51 +0900

    sobal jom

--- scr/App.js
+++ scr/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, StyleSheet } from 'react-native';
 
 const App = () => {
+    //dfdfdf
     let name = '태현'
     return (
         <View style={styles.container}>
