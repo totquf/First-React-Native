d36587cd0f733ea5bdc2c5fb96be09f2f76a368f
totquf <dalda117@naver.com>
2023-02-23 00:53:37 +0900

    props공부

--- scr/App.js
+++ scr/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Text, View, StyleSheet } from 'react-native';
+import MyButton from "./MyButton";
+
+const onPress = () => {
+    return alert("버튼이 클릭되었습니다")
+}
 
 const App = () => {
-    //dfdfdf
-    let name = '태현'
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                {(() => {
-                    if (name == "태현") return "태현입니다"
-                    else if (name == "서경") return "서경입니다"
-                    else return "이름이 없습니다"
-                })()}
-            </Text>
+            <Text style={styles.text}>커스텀 버튼 컴포넌트</Text>
+            <MyButton title='MyButton' onPress={onPress} />
+
+            {/*태그 사이에 지정된 값(Children Props)은 자식 컴포넌트(TouchableOpacity,Text)의 props에 childen이라는 이름으로 전달됨*/}
+            <MyButton title='MyButton' onPress={onPress}>Children Props</MyButton>
+
+            <MyButton onPress={onPress} />
         </View>
     )
 }
@@ -25,8 +28,9 @@ const styles = StyleSheet.create({
         backgroundColor: "#ffffff",
     },
     text: {
-        fontSize: 30,
-    }
+        fontSize: 20,
+        marginBottom: 10,
+    },
 })
 
 export default App;
---
+++ scr/MyButton.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import propTypes from 'prop-types';
+
+//커스텀 버튼 컴포넌트
+const MyButton = ({title, onPress, children}) => {
+    return (
+        //onPress속성을 사용가능한 컴포넌트
+        <TouchableOpacity
+            style={styles.button}
+            onPress={() => onPress}> 
+            <Text style={styles.button_text}> 
+                {children || title}
+            </Text>
+        </TouchableOpacity>  
+    )
+}
+
+//컴포넌트의 props에 기본값을 지정해서 props가 없을경우 기본값을 출력
+MyButton.defaultProps = {
+    title: 'buttton',
+}
+
+//PropTypes을 통해 컴포넌트(MyButton)에서 전달받아야 하는 props의 타입.필수여부를 지정할 수 있음
+MyButton.propTypes = {
+    title: propTypes.string.isRequired,
+    onPress: propTypes.func.isRequired,
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#3498DB',
+        padding: 14,
+        margin: 10,
+        borderRadius: 8,
+    },
+    button_text: {
+        fontSize: 24,
+        color: '#fff',
+    },
+
+})
+
+export default MyButton;
